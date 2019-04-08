5c9f6eb77fa2f47b33a03535590e80dda2654271
Peter-Searby <peter.searby@protonmail.com>
2019-04-08 22:42:24 +0100

    adding a vague test which just opens the game

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import processing.core.PApplet;
+
+public class MainTest {
+
+    @Test
+    public void vagueTest() {
+        String[] appletArgs = new String[] { "RocketResourceRace" };
+        PApplet.main(appletArgs);
+    }
+}
